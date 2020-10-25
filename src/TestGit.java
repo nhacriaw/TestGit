d9b84d06139c663958a55690e381f08f36a6f754
n11 <wased1009@gmail.com>
2020-10-25 23:23:20 +0700

    v3

--- src/TestGit.java
+++ src/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
     public static void main(String[] args) {
         v1();
         v2();
+        v3();
         
     }
     
@@ -12,7 +13,11 @@ public class TestGit {
     }
     
     public static void v2(){
-        System.out.println("V1");
+        System.out.println("V2");
+    }
+    
+    public static void v3(){
+        System.out.println("V3");
     } 
     
 }
